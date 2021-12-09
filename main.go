3ea51dd1de222b312a1121c630d6ef80fc64759d
82901002 <95663935+82901002@users.noreply.github.com>
2021-12-09 19:49:31 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	
+	"log"
 	"os"
 	"towelong/mogu/service"
 	"towelong/mogu/utils"
@@ -18,7 +18,10 @@ func main() {
 	planID := moguding.GetPlanID(token, userId)
 	isSuccess, types := moguding.SignIn(token, planID, userId)
 	title, message := utils.EnumToMsg(types)
-	
+	if !isSuccess {
+		service.SendMessage(title, message)
+		log.Fatal(title)
+	}
 	if service.SendMessage(title, message) {
 		fmt.Println("打卡成功")
 	}
