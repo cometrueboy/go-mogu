3c2be0c82c35f15193e32223f8bcdb93f3cb7cfd
82901002 <95663935+82901002@users.noreply.github.com>
2021-12-09 19:46:03 +0800

    Update main.go

--- main.go
+++ main.go
@@ -18,10 +18,7 @@ func main() {
 	planID := moguding.GetPlanID(token, userId)
 	isSuccess, types := moguding.SignIn(token, planID, userId)
 	title, message := utils.EnumToMsg(types)
-	if !isSuccess {
-		service.SendMessage(title, message)
-		log.Fatal(title)
-	}
+	
 	if service.SendMessage(title, message) {
 		fmt.Println("打卡成功")
 	}
